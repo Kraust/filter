057aeea9a9afc8f7ce3247931b03a3ac7543af40
Kraust <secretdragoon@gmail.com>
2021-11-11 22:21:43 -0500

    Added kraust.filter

---
+++ copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ${USER}/.local/share/Steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My Documents/MyGames/Path of Exile
+
+FILTER_DIR="${HOME}/.local/share/Steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My Documents/My Games/Path of Exile"
+FILTER=./kraust.filter
+
+cp "${FILTER}" "${FILTER_DIR}"
+
